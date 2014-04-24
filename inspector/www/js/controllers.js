33b59f905698e87bb2a229a6cd4e0b3660a02602
railty <railty5@gmail.com>
2014-04-24 16:27:14 -0400

    update

--- inspector/www/js/controllers.js
+++ inspector/www/js/controllers.js
@@ -1,62 +1,9 @@
 angular.module('starter.controllers', [])
 
-.controller('GreetingController', ['$scope', function($scope) {
-  $scope.greeting = 'Hola!';
-	$scope.rightButtons = [
-    { 
-      text: 'Open side navigation',
-      type: 'button-positive',
-      content: '<i class="icon ion-navicon"></i>',
-      click: function(e) {
-        $scope.sideMenuController.toggleRight();
-      }
-    }
-  ];
-	
-}])
-
-.controller('TabCtrl', function($scope, $rootScope) {
-	$rootScope.rightButtons = [
-    { 
-      text: 'Open side navigation',
-      type: 'button-positive',
-      content: '<i class="icon ion-navicon"></i>',
-      click: function(e) {
-        $scope.sideMenuController.toggleRight();
-      }
-    }
-  ];
-	$scope.rightButtons = [
-    { 
-      text: 'Open side navigation',
-      type: 'button-positive',
-      content: '<i class="icon ion-navicon"></i>',
-      click: function(e) {
-        $scope.sideMenuController.toggleRight();
-      }
-    }
-  ];
-})
-
-.controller('DashCtrl', function($scope, $rootScope) {
-
-	$rootScope.headerTitle = 'My Modified Title';
-	
-	$rootScope.rightButtons = [{ 
-	  type: 'button-clear',
-	  content: 'Right Button',
-	  tap: function(e) {
-	    $location.path("/");
-	  }
-	}];
-	$scope.rightButtons = [{ 
-	  type: 'button-clear',
-	  content: 'Right Button',
-	  tap: function(e) {
-	    $location.path("/");
-	  }
-	}];
-	
+.controller('HomeCtrl', function($scope) {
+	$scope.test = function(){
+		$scope.gmap.set_center();
+	}
 })
 
  .controller('InspectionsCtrl', function($scope, Inspection) {
--- inspector/www/js/directives.js
+++ inspector/www/js/directives.js
@@ -1,35 +1,40 @@
 angular.module('starter.directives', [])
 .directive('gmap', function() {
 	
-	function HomeControl(controlDiv, map) {
-  // Set CSS styles for the DIV containing the control
-  // Setting padding to 5 px will offset the control
-  // from the edge of the map.
-  controlDiv.style.padding = '5px';
-
-  // Set CSS for the control border.
-  var controlUI = document.createElement('div');
-  controlUI.style.backgroundColor = 'lightblue';
-  controlUI.style.border = 'solid black 1px';
-  controlUI.style.cursor = 'pointer';
-  controlUI.style.textAlign = 'center';
-  controlUI.title = 'Click to set the map to Home';
-  controlDiv.appendChild(controlUI);
-
-  // Set CSS for the control interior.
-  var controlText = document.createElement('div');
-	controlText.style.padding = '4px 10px 4px 10px';
-  controlText.innerHTML = '<i class="icon icon ion-android-locate"></i>';
-  controlUI.appendChild(controlText);
-
-  // Setup the click event listeners: simply set the map to Chicago.
-  google.maps.event.addDomListener(controlUI, 'click', function() {
+	function set_center(map) {
 		navigator.geolocation.getCurrentPosition(function(pos) {
-				map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-			}, function(error) {
-				alert('Unable to get location: ' + error.message);
-			});
+			map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+		}, function(error) {
+			alert('Unable to get location: ' + error.message);
 		});
+	}
+		
+	function create_center_button(map) {
+		var controlDiv = document.createElement('div');
+		// Set CSS styles for the DIV containing the control
+		// Setting padding to 5 px will offset the control
+		// from the edge of the map.
+		controlDiv.style.padding = '5px';
+
+		// Set CSS for the control border.
+		var controlUI = document.createElement('div');
+		controlUI.style.backgroundColor = 'lightblue';
+		controlUI.style.border = 'solid black 1px';
+		controlUI.style.cursor = 'pointer';
+		controlUI.style.textAlign = 'center';
+		controlUI.title = 'Click to set the map to Home';
+		controlDiv.appendChild(controlUI);
+
+		// Set CSS for the control interior.
+		var controlText = document.createElement('div');
+		controlText.style.padding = '4px 10px 4px 10px';
+		controlText.innerHTML = '<i class="icon icon ion-android-locate"></i>';
+		controlUI.appendChild(controlText);
+
+		google.maps.event.addDomListener(controlUI, 'click', function(){set_center(map);});
+		
+		controlDiv.index = 1;
+		map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(controlDiv);
 	}
 
 	function initialize_map(div) {
@@ -40,20 +45,23 @@ angular.module('starter.directives', [])
 		};
 		
 		var map = new google.maps.Map(div, mapOptions);
-		var homeControlDiv = document.createElement('div');
-		var homeControl = new HomeControl(homeControlDiv, map);
-
-		homeControlDiv.index = 1;
-		map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(homeControlDiv);
+		//create_center_button(map);
 		
 		return map;
 }
 	
-	function link(scope, element, attrs) {
+	function link(scope, element, attrs, ctrl) {
+		scope.$parent.gmap = ctrl;  
+		
 		scope.map = initialize_map(element.context);
 	}
 	
 	return {
+		controller: function($scope) {
+			this.set_center = function(){
+				set_center($scope.map);
+			}
+		},
 		link: link
 	};
 });
